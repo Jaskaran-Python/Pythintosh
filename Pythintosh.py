b0662fa63a62705122a78c560bf31eb098b51807
Jaskaran_python <j.karan151110@gmail.com>
2024-05-30 15:29:51 +0530

    Add files via upload

---
+++ Pythintosh.py
@@ -0,0 +1,73 @@
+import time
+import sys
+import colorama
+from colorama import Fore
+
+colorama.init()
+print("Pythintosh created by skull._.python")
+time.sleep(0.5)
+print(Fore.GREEN + "Enter any key to boot up")
+a = input("")
+time.sleep(0.25)
+un = input("Enter username: ")
+while un != "admin":
+    print("Incorrect username, try again")
+    un = input("Enter username: ")
+p = input("Enter password: ")
+while p != "123":
+    print("Incorrect password, try again")
+    p = input("Enter password: ")
+time.sleep(0.75)
+print("Pythintosh booting up")
+time.sleep(0.5)
+print("Pythintosh has booted up successfully with no errors")
+time.sleep(0.25)
+print("Welcome to Pythintosh, " + un)
+time.sleep(0.25)
+print("For a list of commands, enter: list of commands for applications. For help enter the command, /info user")
+while True: 
+    i = input("")
+    if i == "/list of commands":
+        print("Enter one of the following commands: /sys specs, /shut down sys, /run app calculator, /info user", "/log updates")
+    elif i == "/shut down sys":
+        time.sleep(2)
+        print("Turning off background processes")
+        time.sleep(0.75)
+        print("Shutting down")
+        sys.exit()
+    elif i == "/info user":
+        print("What would you like help with?")
+        h = input(" ")
+        if h == "calculator":
+            print("You can use commands like: add, sub, mul, div")
+    elif i == "/sys specs":
+        time.sleep(0.25)
+        print("Storage: 2098 bytes")
+        time.sleep(0.5)
+        print("OS: Pythintosh 4.0 ")
+        time.sleep(0.25)
+        print("Made with Visual Studio Code")
+    elif i == "/run app calculator":
+        print("Enter which type of calculation you have to do")
+        tc = str(input(" "))
+        print("Enter first number:")
+        fn = float(input())
+        print("Enter second number:")
+        sn = float(input())
+        if tc == "add":
+            print(fn + sn)
+        elif tc == "sub":
+            print(fn - sn)
+        elif tc == "mul":
+            print(fn * sn)
+        elif tc == "div":
+            print(fn / sn)
+    elif i == "/log update":
+     print("decreased code size by 1900")
+     time.sleep(0.35)
+     print("made os faster, does not need to restart often")
+     time.sleep(0.15)
+     print("reduced code by 61 lines!")
+    else:
+     print("wrong command")
+     i = input("")
